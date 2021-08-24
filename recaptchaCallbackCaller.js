a3232dd27c98803e0132f8f20c33e0039626c491
yoyole123 <53074180+yoyole123@users.noreply.github.com>
2021-08-24 16:55:52 +0300

    Add files via upload

---
+++ recaptchaCallbackCaller.js
@@ -0,0 +1,23 @@
+const callbackFinder = function() {
+    const client = ___grecaptcha_cfg.clients[0];
+    let result = [];
+    let firstLevelKeys = Object.keys(client);
+    firstLevelKeys.forEach((flk) => {
+        if (client[flk] !== undefined) {
+            let secondLevelKeys = Object.keys(client[flk]);
+            secondLevelKeys.forEach((slk) => {
+                if (client[flk][slk] !== undefined && client[flk][slk] !== null) {
+                    let thirdLevelKeys = Object.keys(client[flk][slk]);
+                    if (thirdLevelKeys.includes("callback")) {
+                        result = result.concat([flk, slk]);
+                    }
+                }
+            });
+        }
+    });
+    return result;
+}
+
+const callbackMethodKeys = callbackFinder();
+const recaptchaToken = "<INSERT YOUR TOKEN HERE>";
+window["___grecaptcha_cfg"]["clients"][0][callbackMethodKeys[0]][callbackMethodKeys[1]]["callback"](recaptchaToken)
